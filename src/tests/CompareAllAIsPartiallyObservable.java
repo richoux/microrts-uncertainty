eb434205b4129437d54bf021e1b27c9717b6e518
Florian Richoux <florian.richoux@polytechnique.edu>
2018-09-03 21:02:26 +0200

    Add without min results

--- src/tests/CompareAllAIsPartiallyObservable.java
+++ src/tests/CompareAllAIsPartiallyObservable.java
@@ -79,13 +79,13 @@ public class CompareAllAIsPartiallyObservable {
 
 
     // PhysicalGameState pgs = PhysicalGameState.load("maps/8x8/basesWorkers8x8A.xml", utt);
-    PhysicalGameState pgs = PhysicalGameState.load("maps/16x16/basesWorkers16x16A.xml", utt);
+    // PhysicalGameState pgs = PhysicalGameState.load("maps/16x16/basesWorkers16x16A.xml", utt);
     // PhysicalGameState pgs = PhysicalGameState.load("maps/BWDistantResources32x32.xml", utt);
     // PhysicalGameState pgs = PhysicalGameState.load("maps/BroodWar/(4)BloodBath.scmB.xml", utt);
     // PhysicalGameState pgs = PhysicalGameState.load("maps/8x8/FourBasesWorkers8x8.xml", utt);
     // PhysicalGameState pgs = PhysicalGameState.load("maps/16x16/TwoBasesBarracks16x16.xml", utt);
     // PhysicalGameState pgs = PhysicalGameState.load("maps/NoWhereToRun9x8.xml", utt);
-    // PhysicalGameState pgs = PhysicalGameState.load("maps/DoubleGame24x24.xml", utt);
+    PhysicalGameState pgs = PhysicalGameState.load("maps/DoubleGame24x24.xml", utt);
     GameState gs = new GameState(pgs, utt);
     
     bots.add(new POAdaptiverush(utt, "src/ai/poadaptive/distributions.xml", "src/ai/poadaptive/distribution_woutb.xml", "src/ai/poadaptive/solver_cpp"));
