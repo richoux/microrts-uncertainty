3375d2a367d163a6e9a7c714a459804b6c53ad5e
Florian Richoux <florian.richoux@polytechnique.edu>
2018-11-08 11:56:45 +0900

    Add a 32*32 basic map

--- problem_model/src/main.cpp
+++ problem_model/src/main.cpp
@@ -94,9 +94,9 @@ int main(int argv, char *argc[])
     const int LAMBDA = 5;
     //auto phi_callback = identity();
     //auto phi_callback = flat();
-    //auto phi_callback = logistic( LAMBDA );
+    auto phi_callback = logistic( LAMBDA );
     //auto phi_callback = inverse_logistic( LAMBDA );
-    auto phi_callback = logit( LAMBDA );
+    //auto phi_callback = logit( LAMBDA );
 
     cout << "phi(0.1) = " << phi_callback(0.1)
 	 << ", phi(0.5) = " << phi_callback(0.5)
